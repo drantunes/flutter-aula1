d4f517033d79fcec3cf968c840dbf0aecf1e573c
Diego Roberto Antunes <drantunes@gmail.com>
2021-04-06 19:38:33 -0300

    Adicionada a configuração para o Firebase Emulator (Firebase Local)

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_aula1/services/auth_service.dart';
@@ -9,6 +10,8 @@ initConfigurations() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Firebase
   await Firebase.initializeApp();
+  // Para executar o Firebase no Emulador Local do Firebase descomentar a seguir
+  // FirebaseFirestore.instance.settings = Settings(host: 'localhost:8080', sslEnabled: false);
 
   // GetX Bindings
   Get.lazyPut<ThemeController>(() => ThemeController());
